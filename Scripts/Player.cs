06576d4035b5d196048137cb0f0aff7f30dfaeb6
Aadiyat Ahmad <aadiyat.ahmad@outlook.com>
2026-01-18 21:16:22 -0500

    movement animations

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -7,14 +7,35 @@ public partial class Player : CharacterBody2D
     public float RotationSpeed {get; set;} = 1.5f;
 
     [Export]
-    public float Acceleration {get; set;} = 100.0f;
+    public float Acceleration {get; set;} = 50.0f;
 
     [Export]
-    public float MaxVelocity {get; set;} = 500.0f;
+    public float MaxVelocity {get; set;} = 250.0f;
 
-    private float GetAngleToMouse()
+     private AnimatedSprite2D _animatedSprite;
+
+    public override void _Ready()
     {
-        return (-Transform.Y).AngleTo(GetGlobalMousePosition() - Position);
+        _animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+    }
+
+
+    public override void _PhysicsProcess(double delta)
+    {
+        FaceMouse(delta);
+
+        if (Input.IsActionPressed("move"))
+        {
+            Accelerate(delta);
+            _animatedSprite.Play("move");
+        }
+        else
+        {
+            _animatedSprite.Stop();
+        }
+
+
+        MoveAndSlide();
     }
 
     private void Accelerate(double delta)
@@ -22,17 +43,14 @@ public partial class Player : CharacterBody2D
         Velocity += (-Transform.Y) * Acceleration * (float)delta;
     }
 
-    public override void _PhysicsProcess(double delta)
+    private void FaceMouse(double delta)
     {
-        Rotation += RotationSpeed*Mathf.Sign(GetAngleToMouse())*(float)delta;
-
-        if (Input.IsActionPressed("move"))
-        {
-            Accelerate(delta);
-        }
-
-        MoveAndSlide();
+        Rotation += RotationSpeed * Mathf.Sign(GetAngleToMouse()) * (float)delta;
     }
 
+    private float GetAngleToMouse()
+    {
+        return (-Transform.Y).AngleTo(GetGlobalMousePosition() - Position);
+    }
 
 }
